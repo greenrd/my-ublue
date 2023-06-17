7738b78be68489658dda9df3cbcf439a86c77187
greenrd <greenrd@users.noreply.github.com>
2023-06-17 01:16:00 +0100

    Add VSCodium with batteries included
    Just shell script development for now. More to come.

--- scripts/pre/generate-yafti-configuration.sh
+++ scripts/pre/generate-yafti-configuration.sh
@@ -1,3 +1,33 @@
+#! /bin/bash
+
+# TODO: Using bash to generate bash code is a little tricky in places - switch to something cleaner
+
+# Tweakable things
+export VSCODE_VARIANT=com.vscodium.codium
+export VSCODE_VARIANT_SHORT=VSCodium
+
+# Common code snippets
+export not_present="wc -l | grep '^0$'"
+
+# Computed values
+export VSCODE_CONFIG="~/.var/app/$VSCODE_VARIANT/config/$VSCODE_VARIANT_SHORT/User/settings.json"
+
+# Functions
+mkdir_tree_if_absent () {
+  echo "([[ -d $1 ]] || mkdir --parents $1)"
+}
+
+create_file_if_absent () {
+  local filename=$1
+  shift
+  echo "$(mkdir_tree_if_absent $(dirname $filename)) && ([[ -f $filename ]] ||" "$@" "> $filename)"
+}
+
+in-place_jq () {
+  echo "jq |$1| $2 > $2.tmp && mv $2.tmp $2"|tr "|" "'"
+}
+
+cat > /usr/share/ublue-os/firstboot/yafti.yml <<EOF
 title: Welcome to uBlue
 properties:
   mode: "run-on-change"
@@ -5,7 +35,7 @@ screens:
   first-screen:
     source: yafti.screen.title
     values:
-      title: "Welcome to uBlue (Alpha)"
+      title: "Welcome to my-uBlue (Alpha)"
       icon: "/path/to/icon"
       description: |
         This guided installer will help you get started with your new system.
@@ -28,7 +58,7 @@ screens:
     values:
       title: Missing Flathub Repository (System)
       condition:
-        run: flatpak remotes --system --columns=name | grep flathub | wc -l | grep '^0$'
+        run: flatpak remotes --system --columns=name | grep flathub | $not_present
       description: |
         We have detected that you don't have Flathub's repository on your system. We will now add that repository to your system-wide list.
       actions:
@@ -38,11 +68,45 @@ screens:
     values:
       title: Missing Flathub Repository (User)
       condition:
-        run: flatpak remotes --user --columns=name | grep flathub | wc -l | grep '^0$'
+        run: flatpak remotes --user --columns=name | grep flathub | $not_present
       description: |
         We have detected that you don't have Flathub's repository on your current user account. We will now add that repository to your account.
       actions:
         - run: flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+  check-distrobox-fedora:
+    source: yafti.screen.consent
+    values:
+      title: Missing Fedora Distrobox
+      condition:
+        run: distrobox list | grep fedora | $not_present
+      description: |
+        We have detected that you don't have a Fedora Distrobox set up. We will now create one.
+      actions:
+        - run: distrobox create --image quay.io/fedora/fedora:38 --name fedora
+  check-vscode:
+    source: yafti.screen.consent
+    values:
+      title: Development environment
+      condition:
+        run: flatpak list | grep -F $VSCODE_VARIANT | $not_present
+      description: |
+        We have detected that you don't have $VSCODE_VARIANT_SHORT (the open source build of Visual Studio Code) installed. We will now install that with batteries included.
+      actions:
+        - run: "$(mkdir_tree_if_absent "~/.local/bin") && cp /usr/share/shellcheck ~/.local/bin && chmod +x ~/.local/bin/shellcheck"
+        - run: distrobox enter fedora -- sudo dnf install -y shellcheck
+        - run: |
+            $(create_file_if_absent "$VSCODE_CONFIG" echo '{}') && \
+            $(in-place_jq '.bashIde.shellcheckPath = "~/.local/bin/shellcheck"' $VSCODE_CONFIG) && \
+            $(in-place_jq '.bashIde.explainshellEndpoint = "http://localhost:5000"' $VSCODE_CONFIG)
+        - run: |
+            podman container create --name explainshell -p 5000:5000 ghcr.io/idank/idank/explainshell:master && \
+            podman generate systemd explainshell >es.service && \
+            sed -i -e '/WantedBy=default\.target/d' es.service && \
+            $(mkdir_tree_if_absent "~/.config/systemd/user/app-${VSCODE_VARIANT}-.scope.d") && \
+            mv es.service .config/systemd/user/explainshell.service && \
+            cp /usr/share/vscode.conf .config/systemd/user/app-${VSCODE_VARIANT}-.scope.d && \
+            systemctl --user daemon-reload
+        - run: flatpak install -y --system $VSCODE_VARIANT && flatpak run $VSCODE_VARIANT --install-extension mads-hartmann.bash-ide-vscode
   applications:
     source: yafti.screen.package
     values:
@@ -110,9 +174,6 @@ screens:
       links:
         - "Install More Applications":
             run: /usr/bin/plasma-discover
-        - "Website":
-            run: /usr/bin/xdg-open https://ublue.it
-        - "Join the Discord Community":
-            run: /usr/bin/xdg-open https://discord.gg/XjG48C7VHx
       description: |
-        Thanks for trying uBlue, we hope you enjoy it!
+        Thanks for trying my-uBlue, I hope you enjoy it!
+EOF
